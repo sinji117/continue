35cf0c390ccb21ab10dd80f88a18fecec1847b32
sinji imagawa <yui.ibu.rami@gmail.com>
2020-10-02 10:08:16 +0900

    ビューの変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
- def show 
-  
- end
+  def show
+    @food = current_user.foods
+  end
+
+  def index
+    @food = current_user.foods.where("created_at > ?", Date.today)
+  end
 end
