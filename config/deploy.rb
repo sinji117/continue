7c45582d434b7b66cbea53b2246e7cf5eecb3b09
sinji imagawa <yui.ibu.rami@gmail.com>
2020-10-11 11:52:40 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock "3.14.1"
 set :application, "continue"
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url, "git@github.com:sinji117/https://github.com/sinji117/continue.git"
+set :repo_url, "git@github.com:sinji117/continue.git"
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push("log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads")
