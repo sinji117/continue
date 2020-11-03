789e168e64b0a32d5fd2bfa7c44fceae97f0b933
sinji imagawa <yui.ibu.rami@gmail.com>
2020-11-03 11:36:28 +0900

    週間記録に運動状態を反映

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
     #from = Time.current.at_beginning_of_day
     #to = (from + 6.day).at_end_of_day
     @food = current_user.foods.where(created_at: 1.week.ago.beginning_of_day..Time.zone.now.end_of_day)
-
+    @motions = current_user.motions.where(created_at: 1.week.ago.beginning_of_day..Time.zone.now.end_of_day)
     #@food = current_user.foods.limit(7).order(:start_time)
     #@food = current_user.foods.where(created_at: from...to)
   end
