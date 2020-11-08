b3997bf6b53c6374206c99509e84026e397d0caa
sinji imagawa <yui.ibu.rami@gmail.com>
2020-11-08 11:11:14 +0900

    単体テストの実装

--- spec/factories/motions.rb
+++ spec/factories/motions.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :motion do
-    
+    name { Faker::Name.initials(number: 2) }
+    time { Faker::Number.between(from: 1, to: 100000) }
+    kcal { Faker::Number.between(from: 1, to: 100000) }
+    association :user
   end
 end
--- spec/models/motion_spec.rb
+++ spec/models/motion_spec.rb
@@ -1,5 +1,54 @@
 require 'rails_helper'
 
-RSpec.describe Motion, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Motion do
+  before do
+    @motion = FactoryBot.build(:motion)
+  end
+
+  describe '運動記録の新規登録' do
+    context '新規登録がうまくいくとき' do
+      it "name,time,kcalが存在すれば登録できる" do
+        @motion = FactoryBot.build(:motion)
+        expect(@motion).to be_valid
+      end
+    end
+
+    context '新規登録がうまくいかないとき' do
+      it "nameが空だと登録できない" do
+        @motion.name = ""
+        @motion.valid?
+        expect(@motion.errors.full_messages).to include("Name can't be blank")
+      end
+      it "timeが空では登録できない" do
+        @motion.time = ""
+        @motion.valid?
+        expect(@motion.errors.full_messages).to include("Time can't be blank")
+      end
+      it "kcalが空では登録できない" do
+        @motion.kcal = ""
+        @motion.valid?
+        expect(@motion.errors.full_messages).to include("Kcal can't be blank")
+      end
+      it "kcalが全角で入力されると登録できない" do
+        @motion.kcal = "あああ"
+        @motion.valid?
+        expect(@motion.errors.full_messages).to include("Kcal is not a number")
+      end
+      it "timeが全角で入力されると登録できない" do
+        @motion.time = "あああ"
+        @motion.valid?
+        expect(@motion.errors.full_messages).to include("Time is not a number")
+      end
+      it "kcalが半角英字で入力されると登録できない" do
+        @motion.kcal = "aaa"
+        @motion.valid?
+        expect(@motion.errors.full_messages).to include("Kcal is not a number")
+      end
+      it "timeが半角英字で入力されると登録できない" do
+        @motion.time = "aaa"
+        @motion.valid?
+        expect(@motion.errors.full_messages).to include("Time is not a number")
+      end
+    end
+  end
 end
